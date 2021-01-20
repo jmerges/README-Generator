9a31df19e47ad01291766861f059bda0a62a201f
jmerges bootcamp <jamesmerges1@gmail.com>
2021-01-19 19:55:48 -0800

    Working README generator with full functionality

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+const badges = {
+    MIT: "[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)",
+    GPLv3: "[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)",
+    AGLP: "[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)"
+};
+
 inquirer
     .prompt([
         {
@@ -33,24 +39,51 @@ inquirer
             message: "Enter the test instructions for the project:",
             name: "test"
         },
+        {
+            type: "list",
+            choices: ["MIT","GPLv3","AGPL"],
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub username:",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "Enter your email:",
+            name: "email"
+        }
     ])
         .then(info => {
             console.log(info);
-            readmeString = `
-# TITLE
+            readmeString = `# ${info.title}
+${badges[info.license]}
+## Table of Contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Test](#Test)
+- [License](#License)
+- [Questions](#Questions)
 ## Description
 ${info.description}
-## Installation Instructions
+## Installation
 ${info.installation}
-## Usage Information
+## Usage
 ${info.usage}
-## Contribution Guidelines
+## Contribution
 ${info.contribution}
-## Test Instructions
+## Test
 ${info.test}
-            `
+## License
+Application is covered under the ${info.license} license.
+## Questions
+GitHub: https://github.com/${info.github}  
+If you have additional questions, you can reach me at ${info.email}`;
             fs.writeFile("./output/README.md",readmeString,(err) => {
                 if (err) throw err;
                 console.log('The file has been saved!');
               });
-        })
+        });
--- output/README.md
+++ output/README.md
@@ -1,13 +1,25 @@
-
-# TITLE
+# df
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+## Table of Contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Test](#Test)
+- [License](#License)
+- [Questions](#Questions)
 ## Description
-Jimbabwe
-## Installation Instructions
-Boodle
-## Usage Information
-TJTJTJ
-## Contribution Guidelines
-X6
-## Test Instructions
-AWEsoMePANtS
-            
+df
+## Installation
+df
+## Usage
+df
+## Contribution
+df
+## Test
+df
+## License
+Application is covered under the MIT license.
+## Questions
+GitHub: https://github.com/df  
+If you have additional questions, you can reach me at df
